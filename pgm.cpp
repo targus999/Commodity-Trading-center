64aed118fe08f47f347db2e8060cd4e3f4763ea8
Arsha P P <arshapp23897@gmail.com>
2021-12-15 10:07:15 +0530

    mutiplication

--- pgm.cpp
+++ pgm.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+using namespace std;
+
+void multiplication(int a,int b)
+{
+	int c=a*b;
+	cout<<"product="<<c;
+}
 
 int main()
 {
-cout<<"soniya";
+int a=4,b=6;
+multiplication(a,b);
 return 0;
 }
 
